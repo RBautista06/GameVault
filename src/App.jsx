c0cfcfd901c73dd7226746ab3281254fe4a2585e
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-06-12 18:11:17 +0800

    base foolder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="border flex flex-col items-center justify-center p-10">
+        <h1 className="text-4xl font-bold">GameVault</h1>
+        <h2 className="text-4xl font-bold">GameVault</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
