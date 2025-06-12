27c6bede5a5f97de0fdead34b74fdd322462dfa3
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-06-13 03:06:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GameVault/',
+  base: '/GameVault',
   plugins: [react(), tailwindcss()],
 });
